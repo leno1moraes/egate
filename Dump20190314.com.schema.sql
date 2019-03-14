9f6f5b76e755cd36628d2bb7af92ca0678893847
unknown <lenomoraes12@gmail.com>
2019-03-14 19:07:47 -0400

    versao 20190314v6

--- Dump20190314.com.schema.sql
+++ Dump20190314.com.schema.sql
@@ -18,6 +18,30 @@ USE `egatedatabase`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `tb_action`
+--
+
+DROP TABLE IF EXISTS `tb_action`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_action` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `desname` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_action`
+--
+
+LOCK TABLES `tb_action` WRITE;
+/*!40000 ALTER TABLE `tb_action` DISABLE KEYS */;
+INSERT INTO `tb_action` VALUES (1,'ENTRAR'),(3,'SAIR'),(4,'BLOQUEAR');
+/*!40000 ALTER TABLE `tb_action` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tb_class`
 --
 
@@ -42,6 +66,66 @@ INSERT INTO `tb_class` VALUES (1,'1º A'),(2,'1º B'),(3,'2º A'),(4,'2º B'),(5
 UNLOCK TABLES;
 
 --
+-- Table structure for table `tb_gate`
+--
+
+DROP TABLE IF EXISTS `tb_gate`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_gate` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descode` varchar(45) NOT NULL,
+  `iaction` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_action_gate` (`iaction`),
+  CONSTRAINT `fk_action_gate` FOREIGN KEY (`iaction`) REFERENCES `tb_action` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_gate`
+--
+
+LOCK TABLES `tb_gate` WRITE;
+/*!40000 ALTER TABLE `tb_gate` DISABLE KEYS */;
+INSERT INTO `tb_gate` VALUES (1,'FR69TGA55',1),(2,'YU70YHS60',3);
+/*!40000 ALTER TABLE `tb_gate` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_registrygate`
+--
+
+DROP TABLE IF EXISTS `tb_registrygate`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_registrygate` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `student` int(11) NOT NULL,
+  `data` datetime NOT NULL,
+  `iaction` int(11) NOT NULL,
+  `gate` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_student` (`student`),
+  KEY `fk_action` (`iaction`),
+  KEY `fk_gate` (`gate`),
+  CONSTRAINT `fk_action` FOREIGN KEY (`iaction`) REFERENCES `tb_action` (`id`),
+  CONSTRAINT `fk_gate` FOREIGN KEY (`gate`) REFERENCES `tb_gate` (`id`),
+  CONSTRAINT `fk_student` FOREIGN KEY (`student`) REFERENCES `tb_student` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_registrygate`
+--
+
+LOCK TABLES `tb_registrygate` WRITE;
+/*!40000 ALTER TABLE `tb_registrygate` DISABLE KEYS */;
+INSERT INTO `tb_registrygate` VALUES (1,1,'2019-03-14 17:28:11',1,1),(2,1,'2019-03-14 17:29:51',3,2);
+/*!40000 ALTER TABLE `tb_registrygate` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tb_student`
 --
 
@@ -62,7 +146,7 @@ CREATE TABLE `tb_student` (
   PRIMARY KEY (`id`),
   KEY `fk_class_student` (`desperiodo`),
   CONSTRAINT `fk_class_student` FOREIGN KEY (`desperiodo`) REFERENCES `tb_class` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -71,7 +155,7 @@ CREATE TABLE `tb_student` (
 
 LOCK TABLES `tb_student` WRITE;
 /*!40000 ALTER TABLE `tb_student` DISABLE KEYS */;
-INSERT INTO `tb_student` VALUES (1,'Luana Glaucia','201913032222','03670726101',NULL,'6599986763','lenomoraes12@gmail.com',2,1,0);
+INSERT INTO `tb_student` VALUES (1,'Nivalda Lemos','201913032222','03670726101','20191820QWE','6599986763','lenomoraes12@gmail.com',2,1,1),(3,'Sandra Farias 2','201913033444','03670726101','000000','6599986763','roberto@gmail.com',1,1,0),(4,'Roberto Campus 3','201913033333','03670726101','000000','6599986763','roberto@gmail.com',2,1,1);
 /*!40000 ALTER TABLE `tb_student` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -125,7 +209,7 @@ CREATE TABLE `tb_user` (
 
 LOCK TABLES `tb_user` WRITE;
 /*!40000 ALTER TABLE `tb_user` DISABLE KEYS */;
-INSERT INTO `tb_user` VALUES (2,'leno',1,'2019-03-12 00:00:00','Leno Grazianny',0,'lenomoraes12@gmail.com','123'),(8,'admin',1,'2019-03-13 03:01:44','Administrador 3',1,'adm@gmail.com','admin');
+INSERT INTO `tb_user` VALUES (2,'leno',1,'2019-03-12 00:00:00','Leno Grazianny 4',1,'lenomoraes12@gmail.com','123'),(8,'admin',1,'2019-03-13 03:01:44','Administrador 3',1,'adm@gmail.com','admin');
 /*!40000 ALTER TABLE `tb_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -136,6 +220,79 @@ UNLOCK TABLES;
 --
 -- Dumping routines for database 'egatedatabase'
 --
+/*!50003 DROP PROCEDURE IF EXISTS `sp_registrygate_save` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_registrygate_save`(
+pidgate VARCHAR(45),
+pidticket VARCHAR(45)
+)
+BEGIN
+    DECLARE RET_INT INT;
+    DECLARE ANSWER VARCHAR(45);
+    DECLARE IDSTUDENT INT;
+    DECLARE IDGATE INT;
+    DECLARE ASTATUS INT;
+    DECLARE FUNCTIONGATE VARCHAR(45);
+    
+	/*VERIFICAR SE O TICKET ESTÁ VINCULADO A UM ESTUDANTE*/
+    IF EXISTS (SELECT id FROM tb_student WHERE desid2 = pidticket) THEN
+		SET RET_INT = 1;        
+    ELSE
+		SET RET_INT = 0;        
+    END IF;	
+    
+    IF (RET_INT = 0) THEN
+		SET ANSWER = 'BLOQUEADO';
+        
+	ELSE
+		
+        SET IDSTUDENT = (SELECT id FROM tb_student WHERE desid2 = pidticket);
+        SET IDGATE = (SELECT id FROM tb_gate WHERE descode = pidgate);
+		SET FUNCTIONGATE = (SELECT iaction FROM tb_gate WHERE id = IDGATE);  
+        
+		SET ASTATUS = ( SELECT c.iaction 
+						FROM tb_registrygate c
+						WHERE c.id = (SELECT MAX(a.id)
+									  FROM tb_registrygate a 
+									  INNER JOIN tb_action b ON a.iaction = b.id
+									  WHERE a.student = IDSTUDENT ));        
+        /*
+        1 - ENTRAR
+        3 - SAIR
+        */		
+        IF (FUNCTIONGATE = 1 AND ASTATUS = 3) THEN 
+			/*INSERT INTO tb_registrygate (student, data, iaction, gate)
+			VALUES (IDSTUDENT, NOW(), FUNCTIONGATE, IDGATE);*/
+            
+            SET ANSWER = 'LIBERAR_ENTRADA';
+            
+        ELSEIF (FUNCTIONGATE = 3 AND ASTATUS = 1) THEN 
+			/*INSERT INTO tb_registrygate (student, data, iaction, gate)
+			VALUES (IDSTUDENT, NOW(), FUNCTIONGATE, IDGATE);*/
+            
+            SET ANSWER = 'LIBERAR_SAIDA';
+        
+        ELSE
+			SET ANSWER = 'BLOQUEADO';
+        END IF;
+        
+    END IF;
+    
+    SELECT ANSWER AS ANSWER;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `sp_student_save` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -247,4 +404,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-03-14  0:47:30
+-- Dump completed on 2019-03-14 19:06:45
--- views-cache/student-update.667dc68c726582425fac201c046bd7bb.rtpl.php
+++ views-cache/student-update.667dc68c726582425fac201c046bd7bb.rtpl.php
@@ -1,4 +1,4 @@
-<!-- Content Wrapper. Contains page content -->
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
 <!-- Content Header (Page header) -->
 <section class="content-header">
@@ -27,37 +27,37 @@
         <!-- /.box-header -->
 
         <!-- form start -->
-        <form role="form" action="/admin/student/update/{$student.id}" method="post" enctype="multipart/form-data">
+        <form role="form" action="/admin/student/update/<?php echo htmlspecialchars( $student["id"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" method="post" enctype="multipart/form-data">
           <div class="box-body">
 
               <div class="form-group">
                 <label for="desname">Nome</label>
-                <input type="text" class="form-control" id="desname" name="desname" placeholder="Digite o Nome" value="{$student.desname}" required>
+                <input type="text" class="form-control" id="desname" name="desname" placeholder="Digite o Nome" value="<?php echo htmlspecialchars( $student["desname"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" required>
               </div>
   
               <div class="form-group">
                 <label for="desregistr">Matrícula</label>
-                <input type="text" class="form-control" id="desregistr" name="desregistr" placeholder="Digite a Matrícula" value="{$student.desregistr}" required>
+                <input type="text" class="form-control" id="desregistr" name="desregistr" placeholder="Digite a Matrícula" value="<?php echo htmlspecialchars( $student["desregistr"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" required>
               </div>            
   
               <div class="form-group">
                   <label for="desid1">CPF</label>
-                  <input type="text" class="form-control" id="desid1" name="desid1" placeholder="Digite o CPF" value="{$student.desid1}" required>
+                  <input type="text" class="form-control" id="desid1" name="desid1" placeholder="Digite o CPF" value="<?php echo htmlspecialchars( $student["desid1"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" required>
               </div>
   
               <div class="form-group">
                   <label for="desid2">Código do Ticket</label>
-                  <input type="text" class="form-control" id="desid2" name="desid2" placeholder="Digite o RG" value="{$student.desid2}">
+                  <input type="text" class="form-control" id="desid2" name="desid2" placeholder="Digite o RG" value="<?php echo htmlspecialchars( $student["desid2"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
               </div>            
   
               <div class="form-group">
                   <label for="desphonotice">Telefone para aviso</label>
-                  <input type="text" class="form-control" id="desphonotice" name="desphonotice" placeholder="Digite o Telefone" value="{$student.desphonotice}" required>
+                  <input type="text" class="form-control" id="desphonotice" name="desphonotice" placeholder="Digite o Telefone" value="<?php echo htmlspecialchars( $student["desphonotice"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" required>
               </div>               
   
               <div class="form-group">
                 <label for="desemailnotice">E-mail para aviso</label>
-                <input type="email" class="form-control" id="desemailnotice" name="desemailnotice" placeholder="Digite o e-mail" value="{$student.desemailnotice}" required>
+                <input type="email" class="form-control" id="desemailnotice" name="desemailnotice" placeholder="Digite o e-mail" value="<?php echo htmlspecialchars( $student["desemailnotice"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" required>
               </div>
   
               <div class="form-group">
@@ -65,42 +65,42 @@
                   <select class="form-control" id="desperiodo" name="desperiodo">
                     <option value="0"></option>
                     
-                    {if="$student.desperiodo == '1' "}
+                    <?php if( $student["desperiodo"] == '1'  ){ ?>
                     <option value="1" selected>1º A</option>
-                    {else}
+                    <?php }else{ ?>
                     <option value="1">1º A</option>
-                    {/if}
+                    <?php } ?>
 
                     
-                    {if="$student.desperiodo == '2' "}
+                    <?php if( $student["desperiodo"] == '2'  ){ ?>
                     <option value="2" selected>1º B</option>
-                    {else}
+                    <?php }else{ ?>
                     <option value="2">1º B</option>
-                    {/if}
+                    <?php } ?>
 
-                    {if="$student.desperiodo == '3' "}
+                    <?php if( $student["desperiodo"] == '3'  ){ ?>
                     <option value="3" selected>2º A</option>
-                    {else}
+                    <?php }else{ ?>
                     <option value="3">2º A</option>
-                    {/if}
+                    <?php } ?>
 
-                    {if="$student.desperiodo == '4' "}
+                    <?php if( $student["desperiodo"] == '4'  ){ ?>
                     <option value="4" selected>2º B</option>
-                    {else}
+                    <?php }else{ ?>
                     <option value="4">2º B</option>
-                    {/if}
+                    <?php } ?>
 
-                    {if="$student.desperiodo == '5' "}
+                    <?php if( $student["desperiodo"] == '5'  ){ ?>
                     <option value="5" selected>3º A</option>
-                    {else}
+                    <?php }else{ ?>
                     <option value="5">3º A</option>
-                    {/if}
+                    <?php } ?>
 
-                    {if="$student.desperiodo == '6' "}
+                    <?php if( $student["desperiodo"] == '6'  ){ ?>
                     <option value="6" selected>3º B</option>
-                    {else}
+                    <?php }else{ ?>
                     <option value="6">3º B</option>
-                    {/if}
+                    <?php } ?>
                     
                   </select>
                   <!--<input type="email" class="form-control" id="desperiodo" name="desperiodo" placeholder="Digite o e-mail" required>-->
@@ -108,7 +108,7 @@
   
               <div class="checkbox">
                   <label>
-                    <input type="checkbox" id="desstatus" name="desstatus" value="1" {if="$student.desstatus == 1"}checked{/if}> <bold>Permissão para Entrar</bold>
+                    <input type="checkbox" id="desstatus" name="desstatus" value="1" <?php if( $student["desstatus"] == 1 ){ ?>checked<?php } ?>> <bold>Permissão para Entrar</bold>
                   </label>
               </div>
               <br>
@@ -124,7 +124,7 @@
   
                 <div class="box box-widget">
                     <div class="box-body">
-                      <img class="img-responsive" id="image-preview" {if="$student.dephoto == '1' "} src="/res/photo-student/student-{$student.id}.jpg" {else} src="/res/avatar-profile.png" {/if} alt="Photo" width="100" height="100" style="margin: -5px 0px 0px 120px">   
+                      <img class="img-responsive" id="image-preview" <?php if( $student["dephoto"] == '1'  ){ ?> src="/res/photo-student/student-<?php echo htmlspecialchars( $student["id"], ENT_COMPAT, 'UTF-8', FALSE ); ?>.jpg" <?php }else{ ?> src="/res/avatar-profile.png" <?php } ?> alt="Photo" width="100" height="100" style="margin: -5px 0px 0px 120px">   
                     </div>
                   </div>
   
