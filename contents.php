7348ecb32572354f6e1d43174f95ea0dfee6d6da
unknown <lenomoraes12@gmail.com>
2019-03-18 21:30:26 -0400

    versao 20190318v6

---
+++ contents.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; utf-8">
+  <title>e-Gate</title>
+</head>
+<body>
+
+
+<p style="font-family: Arial, Helvetica, sans-serif; font-size: 16px;"><b>e</b>-Gate Informa </p>
+
+
+<p style="font-family: Arial, Helvetica, sans-serif; font-size: 14px;">Registro de: <b> <?=(isset($_GET['status'])) ? $_GET['status'] : ""?> </b></p>
+
+
+<p style="font-family: Arial, Helvetica, sans-serif; font-size: 14px;">Estudante: <b><?=(isset($_GET['estudante'])) ? $_GET['estudante'] : ""?></b></p>
+
+<p style="font-family: Arial, Helvetica, sans-serif; font-size: 14px;">Data: <b><?=(isset($_GET['datareg'])) ? $_GET['datareg'] : ""?></b></p>
+
+</body>
+</html>
--- emailteste.php
+++ emailteste.php
@@ -38,7 +38,7 @@ $mail->Username = "noreplay.egate@gmail.com";
 $mail->Password = "18032019";
 
 //Set who the message is to be sent from
-$mail->setFrom('noreplay.egate@gmail.com', 'e-Gate Controle via Node JavaScript');
+$mail->setFrom('noreplay.egate@gmail.com', 'e-Gate Controle Teste 5');
 
 //Set an alternative reply-to address
 //$mail->addReplyTo('replyto@example.com', 'First Last');
@@ -49,12 +49,17 @@ $mail->addAddress('lenomoraes12@gmail.com', 'Nome do Usuario');
 //Set the subject line
 $mail->Subject = 'Teste de Envio Aqui';
 
+$paramEstudante = "Onel Moraes";
+$paramStatus = "Entrada Liberada";
+$dataReg = "18/03/2019";
+
 //Read an HTML message body from an external file, convert referenced images to embedded,
 //convert HTML into a basic plain-text alternative body
-$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+//$mail->msgHTML(file_get_contents('contents.php?status='.$paramStatus.'&estudante='.$paramEstudante.'&datareg='.$dataReg), dirname(__FILE__));
+$mail->msgHTML(file_get_contents('contents.php'), dirname(__FILE__), array('%status%' => $paramEstudante, '%estudante%' => $paramStatus, '%datareg%' => $dataReg));
 
 //Replace the plain text body with one created manually
-//$mail->AltBody = 'This is a plain-text message body';
+$mail->AltBody = 'This is a plain-text message body';
 
 //Attach an image file
 //$mail->addAttachment('images/phpmailer_mini.png');
